fc0062794458dfd09c562382b705c0203effb68f
Luizefz <luizefsilva2@gmail.com>
2024-11-08 07:29:20 -0300

    feat: item recognition

--- lib/Screens/scanScreen.dart
+++ lib/Screens/scanScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({Key? key}) : super(key: key);
@@ -13,7 +15,9 @@ class ScanScreen extends StatefulWidget {
 class _ScanScreenState extends State<ScanScreen> {
   Uint8List? _imageData;
   String? _imageUrl;
+  String? _apiResult;
   bool _isUploading = false;
+  List<dynamic> registros = [];
 
   Future<void> _pickImage() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
@@ -21,7 +25,9 @@ class _ScanScreenState extends State<ScanScreen> {
     if (result != null && result.files.single.bytes != null) {
       setState(() {
         _imageData = result.files.single.bytes;
-        _imageUrl = null; // Limpa a URL anterior ao selecionar nova imagem
+        _imageUrl = null;
+        _apiResult = null;
+        _isUploading ? null : _uploadImage();
       });
     } else {
       print("Nenhum arquivo selecionado.");
@@ -36,28 +42,19 @@ class _ScanScreenState extends State<ScanScreen> {
     });
 
     try {
-      // Nome único para o arquivo
       final String fileName = 'image_${DateTime.now().millisecondsSinceEpoch}.png';
 
-      // Faz o upload da imagem para o Supabase
       final response = await Supabase.instance.client.storage.from('dispositivos-bb').uploadBinary(fileName, _imageData!);
 
-      // if (response.error != null) {
-      //   print('Erro ao fazer upload: ${response.error!.message}');
-      //   setState(() {
-      //     _isUploading = false;
-      //   });
-      //   return;
-      // }
-
-      // Obtém a URL pública da imagem
       final String url = Supabase.instance.client.storage.from('dispositivos-bb').getPublicUrl(fileName);
-
+      
       setState(() {
         _imageUrl = url;
       });
 
       print("Imagem carregada com sucesso: $url");
+
+      await _sendImageUrlToApi(url);
     } catch (e) {
       print('Erro ao fazer upload: $e');
     } finally {
@@ -65,6 +62,72 @@ class _ScanScreenState extends State<ScanScreen> {
         _isUploading = false;
       });
     }
+  }
+
+  Future<void> _sendImageUrlToApi(String imageUrl) async {
+    try {
+      final response = await http.post(
+        Uri.parse('https://api-visaocomp.onrender.com/predict/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({"image_url": imageUrl}),
+      );
+
+      if (response.statusCode == 200) {
+        final result = jsonDecode(response.body);
+        setState(() {
+          _apiResult = result.toString();
+        });
+
+        print("Resultado da API: $result");
+      } else {
+        print("Erro ao chamar a API: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Erro na requisição para a API: $e");
+    }
+  }
+
+  List<String> _parseDetectedDevices(Map<String, dynamic> apiResponse) {
+    final detections = apiResponse['devices']['detections'] as List<dynamic>;
+    final List<String> detectedDeviceNames = [];
+
+    for (var detection in detections) {
+      final category = detection['categories'][0]['category_name'];
+      if (category != null) {
+        detectedDeviceNames.add(category.toString().toLowerCase());
+      }
+    }
+    return detectedDeviceNames;
+  }
+
+  Future<void> _fetchRegistros() async {
+    try {
+      final registros = await Supabase.instance.client.from('registros').select();
+
+      print("Dados da tabela registros: $registros");
+
+      //   if (response.error == null) {
+      //     setState(() {
+      //       registros = response.data as List<dynamic>;
+      //     });
+      //     print("Dados da tabela registros: $registros");
+      //   } else {
+      //     print("Erro ao buscar registros: ${response.error!.message}");
+      //   }
+    } catch (e) {
+      print("Erro ao buscar registros: $e");
+    }
+  }
+
+  List<String> _extractDeviceNames(Map<String, dynamic> apiResponse) {
+    final devices = apiResponse['devices'] as List<dynamic>;
+    return devices.map((device) => device[0].toString()).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchRegistros();
   }
 
   @override
@@ -106,17 +169,19 @@ class _ScanScreenState extends State<ScanScreen> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: [
-                                const Text(
-                                  "Roteador Wifi",
-                                  style: TextStyle(
+                                Text(
+                                  _apiResult != null ? _apiResult!.substring(10) : 'Identificando...',
+                                  style: const TextStyle(
                                     color: Colors.white,
                                     fontSize: 24,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
                                 const SizedBox(height: 8),
-                                const Text(
-                                  "Roteadores Wi-Fi com defeito podem ser desmontados para aproveitar componentes como antenas, placas de circuito...",
+                                Text(
+                                  _apiResult != null
+                                      ? 'Este dispositivo com defeito podem ser desmontados para aproveitar componentes de placas de circuito e outros itens valiosos.'
+                                      : 'Identificando...',
                                   style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 16,
@@ -160,7 +225,7 @@ class _ScanScreenState extends State<ScanScreen> {
                         ? const CircularProgressIndicator(
                             valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                           )
-                        : const Text("Sim"),
+                        : const Text("Sim", style: TextStyle(color: Colors.white)),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
                       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
@@ -172,31 +237,19 @@ class _ScanScreenState extends State<ScanScreen> {
                       setState(() {
                         _imageData = null;
                         _imageUrl = null;
+                        _apiResult = null;
                       });
                     },
-                    child: const Text("Não"),
+                    child: const Text("Não", style: TextStyle(color: Colors.white)),
                     style: OutlinedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
                       side: const BorderSide(color: Colors.green),
-                      textStyle: const TextStyle(color: Colors.white),
                     ),
                   ),
                 ],
-              ),
-              if (_imageUrl != null) ...[
-                const SizedBox(height: 20),
-                Text(
-                  "Imagem carregada com sucesso:",
-                  style: TextStyle(color: Colors.white),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  _imageUrl!,
-                  style: TextStyle(color: Colors.blueAccent),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+              )
             ],
+            const SizedBox(height: 20),
           ],
         ),
       ),
